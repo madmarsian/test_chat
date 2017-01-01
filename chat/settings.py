4f9423e9c424008943c9f7a38ec891964066f6a5
Maria Dmitrievich <maria.dmitrievich@leverx.com>
2017-01-01 17:42:53 +0300

    static settings

--- chat/settings.py
+++ chat/settings.py
@@ -127,7 +127,7 @@ STATIC_URL = '/static/'
 
 AUTH_USER_MODEL = 'chat.User'
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 CHANNEL_LAYERS = {
     "default": {
@@ -138,3 +138,7 @@ CHANNEL_LAYERS = {
         "ROUTING": "chat.routing.channel_routing",
     },
 }
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
